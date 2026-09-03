222c49e289a7c41807747ca942e0e211a3ee5f4b
Daniel Allen <dan@aeternagroup.net>
2026-09-03 11:39:49 +0100

    Update to schema version v5.0.1

--- main.tf
+++ main.tf
@@ -176,102 +176,102 @@ locals {
 }
 
 module "mssql_servers" {
-  source        = "git::https://github.com/AeternaModules/azurerm_mssql_server.git?ref=v5.0.0"
+  source        = "git::https://github.com/AeternaModules/azurerm_mssql_server.git?ref=v5.0.1"
   mssql_servers = local.mssql_servers
 }
 
 module "mssql_databases" {
-  source          = "git::https://github.com/AeternaModules/azurerm_mssql_database.git?ref=v5.0.0"
+  source          = "git::https://github.com/AeternaModules/azurerm_mssql_database.git?ref=v5.0.1"
   mssql_databases = local.mssql_databases
   depends_on      = [module.mssql_servers]
 }
 
 module "mssql_failover_groups" {
-  source                = "git::https://github.com/AeternaModules/azurerm_mssql_failover_group.git?ref=v5.0.0"
+  source                = "git::https://github.com/AeternaModules/azurerm_mssql_failover_group.git?ref=v5.0.1"
   mssql_failover_groups = local.mssql_failover_groups
   depends_on            = [module.mssql_servers]
 }
 
 module "mssql_firewall_rules" {
-  source               = "git::https://github.com/AeternaModules/azurerm_mssql_firewall_rule.git?ref=v5.0.0"
+  source               = "git::https://github.com/AeternaModules/azurerm_mssql_firewall_rule.git?ref=v5.0.1"
   mssql_firewall_rules = local.mssql_firewall_rules
   depends_on           = [module.mssql_servers]
 }
 
 module "mssql_outbound_firewall_rules" {
-  source                        = "git::https://github.com/AeternaModules/azurerm_mssql_outbound_firewall_rule.git?ref=v5.0.0"
+  source                        = "git::https://github.com/AeternaModules/azurerm_mssql_outbound_firewall_rule.git?ref=v5.0.1"
   mssql_outbound_firewall_rules = local.mssql_outbound_firewall_rules
   depends_on                    = [module.mssql_servers]
 }
 
 module "mssql_server_dns_aliases" {
-  source                   = "git::https://github.com/AeternaModules/azurerm_mssql_server_dns_alias.git?ref=v5.0.0"
+  source                   = "git::https://github.com/AeternaModules/azurerm_mssql_server_dns_alias.git?ref=v5.0.1"
   mssql_server_dns_aliases = local.mssql_server_dns_aliases
   depends_on               = [module.mssql_servers]
 }
 
 module "mssql_server_extended_auditing_policies" {
-  source                                  = "git::https://github.com/AeternaModules/azurerm_mssql_server_extended_auditing_policy.git?ref=v5.0.0"
+  source                                  = "git::https://github.com/AeternaModules/azurerm_mssql_server_extended_auditing_policy.git?ref=v5.0.1"
   mssql_server_extended_auditing_policies = local.mssql_server_extended_auditing_policies
   depends_on                              = [module.mssql_servers]
 }
 
 module "mssql_server_microsoft_support_auditing_policies" {
-  source                                           = "git::https://github.com/AeternaModules/azurerm_mssql_server_microsoft_support_auditing_policy.git?ref=v5.0.0"
+  source                                           = "git::https://github.com/AeternaModules/azurerm_mssql_server_microsoft_support_auditing_policy.git?ref=v5.0.1"
   mssql_server_microsoft_support_auditing_policies = local.mssql_server_microsoft_support_auditing_policies
   depends_on                                       = [module.mssql_servers]
 }
 
 module "mssql_server_transparent_data_encryptions" {
-  source                                    = "git::https://github.com/AeternaModules/azurerm_mssql_server_transparent_data_encryption.git?ref=v5.0.0"
+  source                                    = "git::https://github.com/AeternaModules/azurerm_mssql_server_transparent_data_encryption.git?ref=v5.0.1"
   mssql_server_transparent_data_encryptions = local.mssql_server_transparent_data_encryptions
   depends_on                                = [module.mssql_servers]
 }
 
 module "mssql_virtual_network_rules" {
-  source                      = "git::https://github.com/AeternaModules/azurerm_mssql_virtual_network_rule.git?ref=v5.0.0"
+  source                      = "git::https://github.com/AeternaModules/azurerm_mssql_virtual_network_rule.git?ref=v5.0.1"
   mssql_virtual_network_rules = local.mssql_virtual_network_rules
   depends_on                  = [module.mssql_servers]
 }
 
 module "mssql_database_extended_auditing_policies" {
-  source                                    = "git::https://github.com/AeternaModules/azurerm_mssql_database_extended_auditing_policy.git?ref=v5.0.0"
+  source                                    = "git::https://github.com/AeternaModules/azurerm_mssql_database_extended_auditing_policy.git?ref=v5.0.1"
   mssql_database_extended_auditing_policies = local.mssql_database_extended_auditing_policies
   depends_on                                = [module.mssql_databases]
 }
 
 module "mssql_job_agents" {
-  source           = "git::https://github.com/AeternaModules/azurerm_mssql_job_agent.git?ref=v5.0.0"
+  source           = "git::https://github.com/AeternaModules/azurerm_mssql_job_agent.git?ref=v5.0.1"
   mssql_job_agents = local.mssql_job_agents
   depends_on       = [module.mssql_databases]
 }
 
 module "mssql_jobs" {
-  source     = "git::https://github.com/AeternaModules/azurerm_mssql_job.git?ref=v5.0.0"
+  source     = "git::https://github.com/AeternaModules/azurerm_mssql_job.git?ref=v5.0.1"
   mssql_jobs = local.mssql_jobs
   depends_on = [module.mssql_job_agents]
 }
 
 module "mssql_job_credentials" {
-  source                = "git::https://github.com/AeternaModules/azurerm_mssql_job_credential.git?ref=v5.0.0"
+  source                = "git::https://github.com/AeternaModules/azurerm_mssql_job_credential.git?ref=v5.0.1"
   mssql_job_credentials = local.mssql_job_credentials
   depends_on            = [module.mssql_job_agents]
 }
 
 module "mssql_job_target_groups" {
-  source                  = "git::https://github.com/AeternaModules/azurerm_mssql_job_target_group.git?ref=v5.0.0"
+  source                  = "git::https://github.com/AeternaModules/azurerm_mssql_job_target_group.git?ref=v5.0.1"
   mssql_job_target_groups = local.mssql_job_target_groups
   depends_on              = [module.mssql_job_agents]
 }
 
 module "mssql_job_schedules" {
-  source              = "git::https://github.com/AeternaModules/azurerm_mssql_job_schedule.git?ref=v5.0.0"
+  source              = "git::https://github.com/AeternaModules/azurerm_mssql_job_schedule.git?ref=v5.0.1"
   mssql_job_schedules = local.mssql_job_schedules
   depends_on          = [module.mssql_jobs]
 }
 
 module "mssql_job_steps" {
-  source          = "git::https://github.com/AeternaModules/azurerm_mssql_job_step.git?ref=v5.0.0"
+  source          = "git::https://github.com/AeternaModules/azurerm_mssql_job_step.git?ref=v5.0.1"
   mssql_job_steps = local.mssql_job_steps
   depends_on      = [module.mssql_jobs, module.mssql_job_target_groups]
 }
